2684f0314e704f1875dd3a6fd8be501b8286d0da
Ayoob Bhat <ayoob324005@gmail.com>
2024-01-11 16:28:23 +0530

    added new branch full stack

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require ('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello ')
 })
 
-app.listen(port, () => {
+app.get('/twitter',(req,res)=>{
+  res.send('hii twitter')
+})
+
+
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
